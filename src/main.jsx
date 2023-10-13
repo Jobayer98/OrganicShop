b49a6db8c7ad3e1b43732856c83c804714032f13
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-13 16:49:13 +0600

    add product, cart and user provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
+import ProductProvider from "./context/ProductContext";
+import { Toaster } from "react-hot-toast";
+import CartProvider from "./context/cartContext";
+import UserProvider from "./context/Usercontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ProductProvider>
+      <CartProvider>
+        <UserProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </UserProvider>
+      </CartProvider>
+    </ProductProvider>
   </React.StrictMode>
 );
