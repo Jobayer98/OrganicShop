88ce94cf7d29f6fd2a00280b3db2ed85fed1c3fe
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-16 04:13:47 +0600

    add production server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://organic-shop-o5bhik6cu-jobayer98.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
